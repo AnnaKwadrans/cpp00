70891f7df32523ed35384eaac15fcfae9e7b0170
Anna Kwadrans <akwadran@c1r17s4.42madrid.com>
2025-10-19 21:53:01 +0200

    intra tutorials

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
 
+class Message
+{
+    public:
+        std::string msg;
+        void    toupperMsg(std::string msg);
+};
+
+void    Message::toupperMsg(std::string msg)
+{
+    //for ()
+}
 
 int	main(int argc, char** argv)
 {
